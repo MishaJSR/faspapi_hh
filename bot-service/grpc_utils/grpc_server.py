9ce3012a59ff59fa562653c6ce1605cc0bf0f1ec
develophub <mixan_45@mail.ru>
2024-11-05 14:02:01 +0300

    1.0.35 docker check

--- bot-service/grpc_utils/grpc_server.py
+++ bot-service/grpc_utils/grpc_server.py
@@ -4,12 +4,13 @@ import grpc
 
 from grpc_utils import message_pb2_grpc, message_pb2
 from grpc_utils.MessageService import MessageService
+from base_settings import base_settings
 
 
 async def serve(bot):
     server = grpc.aio.server()
     message_pb2_grpc.add_MessageServiceServicer_to_server(MessageService(bot), server)
-    server.add_insecure_port('tg-bot:50051')
+    server.add_insecure_port(f'{base_settings.get_grpc_host()}:50051')
     logging.info("server grpc start")
     await server.start()
     await server.wait_for_termination()
